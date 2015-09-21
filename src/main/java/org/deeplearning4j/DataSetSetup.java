ab53f4b792374f440f00a38824eefcd5c5e22dce
Adam Gibson <adam@skymind.io>
2015-09-21 09:52:12 -0700

    model

---
+++ src/main/java/org/deeplearning4j/DataSetSetup.java
@@ -0,0 +1,184 @@
+package org.deeplearning4j;
+
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.split.FileSplit;
+import org.canova.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.SamplingDataSetIterator;
+import org.deeplearning4j.datasets.rearrange.LocalUnstructuredDataFormatter;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.StandardScaler;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by agibsonccc on 9/21/15.
+ */
+public class DataSetSetup {
+
+    // Path to the labeled images
+
+    List<String> labels = new ArrayList<>(Arrays.asList("beach", "desert", "forest", "mountain", "rain", "snow"));
+    //List<String> labels = new ArrayList<>(Arrays.asList("mountain", "rain"));
+    final int numRows = 75;
+    final int numColumns = 75;
+    int nChannels = 3;
+    int outputNum = labels.size();
+    int batchSize = 1000;
+    int iterations = 1;
+    int seed = 123;
+    private DataSetIterator trainIter,testIter;
+    private MultiLayerConfiguration conf;
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public void setConf() {
+        //setup the network
+        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .batchSize(batchSize)
+                .iterations(iterations).regularization(true)
+                .l1(1e-1).l2(2e-4).useDropConnect(true)
+                .constrainGradientToUnitNorm(true).miniBatch(true)
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .list(6)
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        .nOut(5).dropOut(0.5)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation("relu")
+                        .build())
+
+                .layer(1, new SubsamplingLayer
+                        .Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(3, 3)
+                        .nOut(10).dropOut(0.5)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation("relu")
+                        .build())
+                .layer(3, new SubsamplingLayer
+                        .Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .build())
+                .layer(4, new DenseLayer.Builder().nOut(100).activation("relu")
+                        .build())
+
+                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nOut(outputNum)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation("softmax")
+                        .build())
+                .backprop(true).pretrain(false);
+
+        new ConvolutionLayerSetup(builder,numRows,numColumns,nChannels);
+        conf = builder.build();
+    }
+
+
+    public void setup() throws Exception {
+        StandardScaler scaler = new StandardScaler();
+
+        Nd4j.factory().setDType(DataBuffer.Type.FLOAT);
+        Nd4j.dtype = DataBuffer.Type.FLOAT;
+        String labeledPath = System.getProperty("user.home")+ File.separator + "data";
+        String testTrainSplitPath = System.getProperty("user.home") + "/splittesttrain";
+        File splitTestTrainRoot = new File(testTrainSplitPath);
+        if(!splitTestTrainRoot.exists()) {
+            LocalUnstructuredDataFormatter formatter = new LocalUnstructuredDataFormatter(splitTestTrainRoot,new File(labeledPath), LocalUnstructuredDataFormatter.LabelingType.DIRECTORY,0.8);
+            formatter.rearrange();
+        }
+
+
+        RecordReader trainReader = new ImageRecordReader(numRows,numColumns,nChannels,true);
+        trainReader.initialize(new FileSplit(new File(new File(splitTestTrainRoot, "split"), "train")));
+        DataSetIterator iter;
+        File meanFile = new File("mean.bin");
+        File stdFile = new File("std.bin");
+        if(!meanFile.exists() || !stdFile.exists()) {
+            iter   = new RecordReaderDataSetIterator(trainReader,10000,numColumns * numRows * nChannels,6);
+            scaler.fit(iter.next());
+            scaler.save(meanFile,stdFile);
+        }
+        else {
+            scaler.load(meanFile,stdFile);
+        }
+
+        RecordReader testReader = new ImageRecordReader(numRows,numColumns,nChannels,true);
+        testReader.initialize(new FileSplit(new File(new File(splitTestTrainRoot, "split"), "test")));
+
+
+        System.out.println("Begin training");
+        DataSet trainingSet;
+        File training = new File("train.bin");
+        if(!training.exists()) {
+            iter = new RecordReaderDataSetIterator(trainReader,10000,numColumns * numRows * nChannels,6);
+            trainingSet = iter.next();
+            trainingSet.save(training);
+        }
+        else {
+            trainingSet = new DataSet();
+            trainingSet.load(training);
+        }
+
+        trainIter = new SamplingDataSetIterator(trainingSet,100,10000);
+        System.out.println("Loading test data");
+        DataSet testNext = null;
+        File testSet = new File("test.bin");
+        if(!testSet.exists()) {
+            testIter = new RecordReaderDataSetIterator(trainReader,10000,numColumns * numRows * nChannels,6);
+            testNext = testIter.next();
+            testNext.save(testSet);
+        }
+        else {
+            testNext = new DataSet();
+            testNext.load(testSet);
+        }
+
+        trainingSet.shuffle();
+
+
+        scaler.transform(trainingSet);
+        scaler.transform(testNext);
+
+
+
+    }
+
+    public MultiLayerConfiguration getConf() {
+        return conf;
+    }
+
+    public DataSetIterator getTrainIter() {
+        return trainIter;
+    }
+
+    public void setTrainIter(DataSetIterator trainIter) {
+        this.trainIter = trainIter;
+    }
+
+    public DataSetIterator getTestIter() {
+        return testIter;
+    }
+
+    public void setTestIter(DataSetIterator testIter) {
+        this.testIter = testIter;
+    }
+}
---
+++ src/main/java/org/deeplearning4j/SparkLocal.java
@@ -0,0 +1,116 @@
+package org.deeplearning4j;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.feature.StandardScaler;
+import org.apache.spark.mllib.feature.StandardScalerModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.canova.image.recordreader.ImageRecordReader;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.util.MLLibUtil;
+import org.deeplearning4j.util.StringUtils;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import scala.Tuple2;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class SparkLocal {
+    public static void main( String[] args) throws Exception {
+        final JavaSparkContext sc = new JavaSparkContext(new SparkConf().setMaster("local[*]").setAppName("scenes"));
+        DataSetSetup setSetup = new DataSetSetup();
+        setSetup.setup();
+        setSetup.getConf();
+
+        JavaRDD<LabeledPoint> data = MLLibUtil.fromDataSet(sc,sc.parallelize(setSetup.getTrainIter().next().asList());
+        StandardScaler scaler = new StandardScaler(true,true);
+
+        final StandardScalerModel scalarModel = scaler.fit(data.map(new Function<LabeledPoint, Vector>() {
+            @Override
+            public Vector call(LabeledPoint v1) throws Exception {
+                return v1.features();
+            }
+        }).rdd());
+        //get the trained data for the train/test split
+        JavaRDD<LabeledPoint> normalizedData = data.map(new Function<LabeledPoint, LabeledPoint>() {
+            @Override
+            public LabeledPoint call(LabeledPoint v1) throws Exception {
+                Vector features = v1.features();
+                Vector normalized = scalarModel.transform(features);
+                return new LabeledPoint(v1.label(), normalized);
+            }
+        }).cache();
+
+
+        //train test split 60/40
+        JavaRDD<LabeledPoint>[] trainTestSplit = normalizedData.randomSplit(new double[]{80, 20});
+
+
+
+        MultiLayerConfiguration conf = setSetup.getConf();
+        //train the network
+        SparkDl4jMultiLayer trainLayer = new SparkDl4jMultiLayer(sc.sc(),conf);
+        //fit on the training set
+        MultiLayerNetwork trainedNetwork = trainLayer.fit(trainTestSplit[0],100);
+        final SparkDl4jMultiLayer trainedNetworkWrapper = new SparkDl4jMultiLayer(sc.sc(),trainedNetwork);
+
+        // Compute raw scores on the test set.
+        JavaRDD<Tuple2<Double, Double>> predictionAndLabels = trainTestSplit[1].map(
+                new Function<LabeledPoint, Tuple2<Double, Double>>() {
+                    public Tuple2<Double, Double> call(LabeledPoint p) {
+                        Vector prediction = trainedNetworkWrapper.predict(p.features());
+                        double max = 0;
+                        double idx = 0;
+                        for(int i = 0; i < prediction.size(); i++) {
+                            if(prediction.apply(i) > max) {
+                                idx = i;
+                                max = prediction.apply(i);
+                            }
+                        }
+
+                        return new Tuple2<>(idx, p.label());
+                    }
+                }
+        );
+
+        System.out.println("Saving model...");
+
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("model.bin"));
+        Nd4j.write(bos,trainedNetwork.params());
+        FileUtils.write(new File("conf.yaml"),trainedNetwork.conf().toYaml());
+
+
+
+        // Get evaluation metrics.
+      /*  MulticlassMetrics metrics = new MulticlassMetrics(predictionAndLabels.rdd());
+        double precision = metrics.fMeasure();
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("model.bin"));
+        Nd4j.write(bos,trainedNetwork.params());
+*/       // FileUtils.write(new File("conf.yaml"),trainedNetwork.conf().toYaml());
+        //System.out.println("F1 = " + precision);
+
+
+    }
+}
