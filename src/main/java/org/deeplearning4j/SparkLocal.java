28faa650c3b7d3e07448fd527f72e1dcc65e8e3b
Adam Gibson <adam@skymind.io>
2015-09-21 18:42:47 -0700

    update

--- src/main/java/org/deeplearning4j/SparkLocal.java
+++ src/main/java/org/deeplearning4j/SparkLocal.java
@@ -59,7 +59,7 @@ public class SparkLocal {
         //System.out.println("Loaded " + next.numExamples() + " with num features " + next.getLabels().columns());
 
 
-        JavaRDD<DataSet> dataSetJavaRDD = sc.parallelize(list,list.size() / 100);
+        JavaRDD<DataSet> dataSetJavaRDD = sc.parallelize(list,list.size() / 10);
 
 
         //train test split 60/40
